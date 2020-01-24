1a21345d3f46fe2bba40e39a7234b78735dbab26
lumsdenitservices <59616032+lumsdenitservices@users.noreply.github.com>
2020-01-24 15:34:33 +0000

    Add Orders

---
+++ src/main/java/com/example/demo/dto/Order.java
@@ -0,0 +1,47 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+	
+	private int id;
+	private String name;
+	private int price;
+	
+	public Order() {
+		
+	}
+	
+	public Order(int id, String name, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
--- src/main/java/com/example/demo/resource/ApplicationResource.java
+++ src/main/java/com/example/demo/resource/ApplicationResource.java
@@ -2,16 +2,27 @@ package com.example.demo.resource;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.OrderService;
 
 @Component
 public class ApplicationResource extends RouteBuilder{
 
+	@Autowired
+	private OrderService service;
+	
 	@Override
 	public void configure() throws Exception {
 		restConfiguration().component("servlet").port(9090).host("localhost").bindingMode(RestBindingMode.json);
 		
 		rest().get("/hellow-world").produces(MediaType.APPLICATION_JSON_VALUE).route().setBody(constant("Welcome"));
+		
+		rest().get("/getOrders").produces(MediaType.APPLICATION_JSON_VALUE).route().setBody(()->service.getOrders());
 	}
+	
+	
 }
